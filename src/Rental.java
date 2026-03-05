a02aaf09cf770ae2c061d7bd461555beadd585c4
ErosJohnLao <erosjohn.lao@cit.edu>
2026-03-05 20:01:45 +0800

    Rental Class

--- src/Rental.java
+++ src/Rental.java
@@ -1,3 +1,30 @@
 public class Rental {
 
+    private String rentalId;
+    private Customer customer;
+    private Bike bike;
+    private int hours;
+    private double totalCost;
+
+    public Rental(String rentalId, Customer customer, Bike bike, int hours) {
+        this.rentalId = rentalId;
+        this.customer = customer;
+        this.bike = bike;
+        this.hours = hours;
+        this.totalCost = bike.calculateRentalCost(hours);
+        bike.setAvailable(false);
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void displayRental() {
+        System.out.println("===== RENTAL DETAILS =====");
+        System.out.println("Rental ID: " + rentalId);
+        customer.displayCustomer();
+        bike.displayInfo();
+        System.out.println("Hours: " + hours);
+        System.out.println("Total Cost: " + totalCost);
+    }
 }
