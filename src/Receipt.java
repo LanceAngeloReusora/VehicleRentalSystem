5c435224bc903b5e5f7040fc2d97f920a33ee612
LanceAngeloReusora <lanceangelo.reusora@cit.edu>
2026-03-05 22:14:09 +0800

    Receipt update

--- src/Receipt.java
+++ src/Receipt.java
@@ -1,3 +1,8 @@
 public class Receipt {
 
+    public static void printReceipt(Rental rental) {
+        System.out.println("\n===== RECEIPT =====");
+        rental.displayRental();
+        System.out.println("====================\n");
+    }
 }
