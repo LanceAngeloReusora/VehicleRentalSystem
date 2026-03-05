65610de2fe5ddd9e731a8e19ff598b50df06c6cf
ErosJohnLao <erosjohn.lao@cit.edu>
2026-03-05 19:15:59 +0800

    Customer class getter

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,30 @@
 public class Customer {
 
+    private String customerId;
+    private String name;
+    private String contact;
+
+    public Customer(String customerId, String name, String contact) {
+        this.customerId = customerId;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+    return contact;
+    }
+
+    public void displayCustomer() {
+        System.out.println("Customer ID: " + customerId +
+                " | Name: " + name +
+                " | Contact: " + contact);
+    }
 }
