c2f90af4e24ad893a68c507321c181caf94093ba
jrnlrno <jehryn.laurino@cit.edu>
2026-03-05 20:16:52 +0800

    BMXBike.java

---
+++ src/BMXBike.java
@@ -0,0 +1,11 @@
+public class BMXBike extends Bike {
+
+    public BMXBike(String id, String brand) {
+        super(id, brand, 40); 
+    }
+
+    @Override
+    public double calculateRentalCost(int hours) {
+        return getRatePerHour() * hours;
+    }
+}
