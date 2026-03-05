218fb83a150cba831bb7af1a062199ecc3f5d7e8
ivancarl <ivancarl.salvacion@cit.edu>
2026-03-05 22:36:10 +0800

    added code

--- src/JapaneseBike.java
+++ src/JapaneseBike.java
@@ -1,3 +1,10 @@
-public class JapaneseBike {
-
+public class JapaneseBike extends Bike {
+    public JapaneseBike(String id, String brand) {
+        super(id, brand, 45); 
+    }
+    
+    @Override
+    public double calculateRentalCost(int hours) {
+        return getRatePerHour() * hours;
+    }
 }
